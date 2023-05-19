4b28b2cd68ba03d90b7843807c38823db2698455
Renan Gustavo <renandevfullstack@gmail.com>
2023-05-19 14:04:52 -0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,19 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'primary-white': '#F9F9F9',
+      'primary-black': '#212429',
+      'secondary-color': '#A4A4A',
+  
+      'text': '#828282',
+      'green': '#00DF5E',
+      'gray': '#333333',
+      'dark-gray': '#212121',
+      'very-dark-gray': '#171717',
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+
     },
   },
   plugins: [],
